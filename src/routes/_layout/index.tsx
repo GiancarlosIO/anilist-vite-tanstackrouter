3ca16f8253134f318232c17959fbe2b51253a630
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2024-01-20 18:37:07 -0500

    feat: create homepage

--- src/routes/_layout/index.tsx
+++ src/routes/_layout/index.tsx
@@ -1,9 +1,23 @@
-import { FileRoute } from '@tanstack/react-router';
+import { Button } from '@/components/ui/button';
+import { FileRoute, Link } from '@tanstack/react-router';
 
 export const Route = new FileRoute('/_layout/').createRoute({
   component: IndexRoute,
 });
 
 function IndexRoute() {
-  return <div className="p-2">Home</div>;
+  return (
+    <>
+      <div className="p-12 flex items-center justify-center bg-slate-950 rounded-sm mt-12">
+        <h1 className="text-white text-2xl">
+          The next-generation anime platform
+        </h1>
+      </div>
+      <div className="flex justify-center mt-4">
+        <Button asChild>
+          <Link to="/search/anime">Find animes</Link>
+        </Button>
+      </div>
+    </>
+  );
 }
